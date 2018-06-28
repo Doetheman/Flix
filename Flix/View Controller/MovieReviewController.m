2ad85daa6b3427e5b3c6cab5bc2f765db6d2c444
Dorian Holmes <restlessd@fb.com>
2018-06-28 11:28:52 -0700

    Detail view controller

--- Flix/View Controller/MovieReviewController.m
+++ Flix/View Controller/MovieReviewController.m
@@ -7,11 +7,11 @@
 //
 #import "MovieCell.h"
 #import "MovieReviewController.h"
-
+#import "UIImageView+AFNetworking.h" //more methods :D
 @interface MovieReviewController () <UITableViewDataSource, UITableViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
-
+@property (nonatomic, strong) UIRefreshControl *refreshControl; //Refresh variable
 @property (nonatomic, strong) NSArray *movies;
 
 @end
@@ -25,30 +25,39 @@
     self.tableView.delegate = self;
     // Do any additional setup after loading the view.
     
+    [self fetchMovies];
+    self.refreshControl = [[UIRefreshControl alloc] init]; //initialize refresh control
+    [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
+    [self.tableView insertSubview:self.refreshControl atIndex:0];//add refresh control to table behind the first cell
+    }
+
+- (void)fetchMovies{
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+          // TODO: Get the array of movies
         if (error != nil) {
             NSLog(@"%@", [error localizedDescription]);
         }
         else {
             NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
             
+            // TODO: Store the movies in a property to use elsewhere
+            
             NSLog(@"%@", dataDictionary);
             self.movies = dataDictionary[@"results"];
             for ( NSDictionary *movie in self.movies ) {
                 NSLog(@"%@", movie[@"title"]);
             }
-            [self.tableView reloadData];
-            // TODO: Get the array of movies
-            // TODO: Store the movies in a property to use elsewhere
             // TODO: Reload your table view data
+            [self.tableView reloadData];
         }
+        [self.refreshControl endRefreshing];
     }];
     [task resume];
     
-                      }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -66,8 +75,14 @@
     NSDictionary *movie = self.movies[indexPath.row];
     cell.movieTitleLabel.text = movie[@"title"];
     cell.synopsisLabe.text =movie[@"overview"];
+    //Movie images
     NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
-    NSString *posterURLString = movie
+    NSString *posterURLString = movie[@"poster_path"];
+    NSString *fullPosterURLString = [baseURLString stringByAppendingString: posterURLString];
+    
+    NSURL *posterURL = [NSURL URLWithString:fullPosterURLString]; //Checks url
+    cell.posterView.image = nil; //Rid of previous
+    [cell.posterView setImageWithURL:posterURL]; //Adds in image in cells
     //cell.textLabel.text = movie[@"title"];
    
     return cell;
